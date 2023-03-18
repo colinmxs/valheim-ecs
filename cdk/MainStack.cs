1c52088a15d0f7de574eb02d1d490a0438a71a76
Colin Smith <colinsmith1@boisestate.edu>
2023-03-18 13:48:09 -0600

    rename cdkstack -> mainstack

--- cdk/MainStack.cs
+++ cdk/MainStack.cs
@@ -3,9 +3,9 @@ using Constructs;
 
 namespace Cdk
 {
-    public class CdkStack : Stack
+    public class MainStack : Stack
     {
-        internal CdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        internal MainStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
             // The code that defines your stack goes here
         }
