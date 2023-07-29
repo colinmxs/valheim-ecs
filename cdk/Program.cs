fc2c17e4e9371a9da98c3c8175ed18a4fcb64eec
Colin Smith <7762103+colinmxs@users.noreply.github.com>
2023-07-29 17:33:31 -0600

    pass account via param

--- cdk/Program.cs
+++ cdk/Program.cs
@@ -10,7 +10,20 @@ namespace Valheim
         public static void Main(string[] args)
         {
             var app = new App();
-            new MainStack(app, "ValheimStack", new StackProps { });
+            var accountParam = new Amazon.CDK.CfnParameter(app, "Account", new Amazon.CDK.CfnParameterProps
+            {
+                Type = "String",
+                Description = "AWS Account ID"
+            });
+
+            new MainStack(app, "ValheimStack", new StackProps 
+            {
+                Env = new Amazon.CDK.Environment
+                {
+                    Account = accountParam.ValueAsString,
+                    Region = "us-west-2"
+                }
+            });
             app.Synth();
         }
     }
